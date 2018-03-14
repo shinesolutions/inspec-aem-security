04c0df9cb0f24668265ba014cfb048d992816c41
Cliffano Subagio <cliffano@gmail.com>
2018-03-15 10:05:06 +1100

    Update Rubocop config consistent with ruby_aem's. Add comments, copyright headers, and various lint violations cleanup.

--- libraries/aem.rb
+++ libraries/aem.rb
@@ -1,5 +1,21 @@
+# Copyright 2018 Shine Solutions
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require_relative './helper'
 
+# Aem class contains security checks relevant to an AEM instance,
+# used by AEM Author and AEM Publish
 class Aem < Inspec.resource(1)
   include Capybara::DSL
   name 'aem'
@@ -23,15 +39,12 @@ class Aem < Inspec.resource(1)
     aem = init_aem_client(conf).aem
 
     begin
-      result = aem.get_agents(aem_role)
+      aem.get_agents(aem_role)
       has_non_default_admin_password = false
     rescue RubyAem::Error => error
-      if error.result.response.status_code.eql? 401
-        has_non_default_admin_password = true
-      end
+      has_non_default_admin_password = true if error.result.response.status_code.eql? 401
       raise error
     end
     has_non_default_admin_password
   end
-
 end
